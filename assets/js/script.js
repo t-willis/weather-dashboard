f6fc9ec51ad6c27c23e5b68fb97b58cd1845e85e
skybreakbb <willistaylora@gmail.com>
2023-05-19 23:00:38 -0500

    added placeholder text/icons

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,22 +11,25 @@
 var inputZip = "37027";
 var locLat = "36.0063";
 var locLon = "-86.7909";
-var inputCity = "";
+var inputCity = "San Francisco ";
 
 
-// function to fetch lat/lon based on user input for city name or zip code
+// just for zips, might not use
+// http://api.openweathermap.org/geo/1.0/zip?zip={zip code},{country code}&appid={API key}
+// function to fetch lat/lon based on user input for city name
 // function getGeoApi() {
-//     var requestLatLonUrl = "http://api.openweathermap.org/geo/1.0/zip?zip=" + inputZip + "&appid=0dfeca27786a8a8c837f120f88c9a791";
+//     var requestLatLonUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + inputCity + "&appid=0dfeca27786a8a8c837f120f88c9a791";
 
 //     fetch(requestLatLonUrl)
 //         .then(function (response) {
 //             return response.json();
 //         })
 //         .then(function (data) {
-//             locLat = data.lat;
-//             locLon = data.lon;
+//             locLat = data[0].lat;
+//             locLon = data[0].lon;
 //             console.log(locLat);
 //             console.log(locLon);
+//             console.log(data);
 //         })
 // }
 
@@ -35,7 +38,7 @@ var inputCity = "";
 
 // function to fetch weather data based on lat/lon as requested from previous fetch request
 // function getWeather() {
-//     var requestWeatherUrl = "http://api.openweathermap.org/data/2.5/forecast?lat=" + locLat + "&lon=" + locLon + "&units=imperial&appid=0dfeca27786a8a8c837f120f88c9a791";
+//     var requestWeatherUrl = "http://api.openweathermap.org/data/2.5/weather?lat=" + locLat + "&lon=" + locLon + "&units=imperial&appid=0dfeca27786a8a8c837f120f88c9a791";
     
 //     fetch(requestWeatherUrl)
 //     .then(function (response) {
