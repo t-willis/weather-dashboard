df04d8ab8550dc79cac17a09043cf6a94534127b
skybreakbb <willistaylora@gmail.com>
2023-05-20 21:02:36 -0500

    started working on clickEvent to pull text input from form and run each necessary fetch request

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,7 @@
 var inputZip = "37027";
 var locLat = "36.0063";
 var locLon = "-86.7909";
-var inputCity = "San Francisco ";
+// var inputCity = "San Francisco ";
 
 
 // just for zips, might not use
@@ -55,3 +55,67 @@ var inputCity = "San Francisco ";
 // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 // DO NOT DELETE WHAT IS ABOVE YOU NEED IT FOR LATER
 // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+// Empty string to be determined by form input
+var inputCity = "";
+var locLat = "";
+var locLon = "";
+
+
+// eventListener/click event to grab form input, then run api request using city name
+$("#cityInputBtn").on("click", function() {
+    inputCity = $("#cityInput").val();
+    console.log("This is from the form input, #inputCity");
+    console.log(inputCity);
+    function getGeoApi() {
+    var requestLatLonUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + inputCity + "&appid=0dfeca27786a8a8c837f120f88c9a791";
+
+    fetch(requestLatLonUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            locLat = data[0].lat;
+            locLon = data[0].lon;
+            console.log("This is from requestLatLonUrl");
+            console.log("locLat var = " + locLat);
+            console.log("locLon var = " + locLon);
+        })
+    }
+    getGeoApi();
+
+    setTimeout(
+        function getWeather() {
+            var requestWeatherUrl = "http://api.openweathermap.org/data/2.5/weather?lat=" + locLat + "&lon=" + locLon + "&units=imperial&appid=0dfeca27786a8a8c837f120f88c9a791";
+            
+            fetch(requestWeatherUrl)
+            .then(function (response) {
+                JSON.stringify(response);
+                return response.json();
+            })
+            .then(function (data1) {
+                console.log("This is from CURRENT weather request");
+                console.log(data1);
+            })
+        }, 2000
+    );
+
+    setTimeout(
+        function getForecast() {
+            var requestForecastUrl = "http://api.openweathermap.org/data/2.5/forecast?lat=" + locLat + "&lon=" + locLon + "&units=imperial&appid=0dfeca27786a8a8c837f120f88c9a791";
+
+            fetch(requestForecastUrl)
+            .then(function (response) {
+                JSON.stringify(response);
+                return response.json();
+            })
+            .then(function (data2) {
+                console.log("This is from FORECAST weather request");
+                console.log(data2);
+            })
+        }, 2000
+    );
+    
+        
+});
+
