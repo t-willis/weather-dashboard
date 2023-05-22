379b1d49b4ccf7a82171b02c3ea7bf2f24f90e4a
skybreakbb <willistaylora@gmail.com>
2023-05-21 21:46:44 -0500

    cleaned up js to force capital letter for variable inputCity

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,11 +6,13 @@ var locLon = "";
 // dayjs things
 var currentDate = dayjs();
 var dateplus = currentDate.add(1, 'day');
+
+// populate current date
+$("#currentCity").text("Today's Date" + " - " + currentDate.format("MMM D, YYYY"));
 // for loop to populate future dates in 5 day forecast
 for (var i = 1; i < 6; i++) {
     $("#fiveDate" + [i]).text(currentDate.add([i], 'day').format("MMM D, YYYY"));
 }
-$("#currentCity").text("Today's Date" + " - " + currentDate.format("MMM D, YYYY"));
 
 // function to determine direction of wind by degree 0-360
 function getDirection(angle) {
@@ -22,12 +24,15 @@ function getDirection(angle) {
 // eventListener/click event to grab form input, then run api request using city name
 $("#cityInputBtn").on("click", function() {
     inputCity = $("#cityInput").val();
+    inputCity = inputCity.charAt(0).toUpperCase()+ inputCity.slice(1);
     console.log("This is from the form input, #inputCity");
     console.log(inputCity);
     // change button text to 'please wait' while data is being fetched
     $("#cityInputBtn").text("please wait");
     function getGeoApi() {
     var requestLatLonUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + inputCity + "&appid=0dfeca27786a8a8c837f120f88c9a791";
+
+    var prevSearchEl = $('<button id="justatest" class="button is-fullwidth is-link is-small mb-1">' + inputCity + '</button>');
 
     // fetch request to set lat/lon variables
     fetch(requestLatLonUrl)
@@ -37,6 +42,7 @@ $("#cityInputBtn").on("click", function() {
         .then(function (data) {
             locLat = data[0].lat;
             locLon = data[0].lon;
+            $("#previousSearches").append(prevSearchEl);
             console.log("This is from requestLatLonUrl");
             console.log("locLat var = " + locLat);
             console.log("locLon var = " + locLon);
@@ -57,7 +63,7 @@ $("#cityInputBtn").on("click", function() {
             .then(function (data1) {
                 console.log("This is from CURRENT weather request");
                 console.log(data1);
-                $("#currentCity").text(inputCity.charAt(0).toUpperCase() + inputCity.slice(1) + " - " + dayjs().format("MMM D, YYYY"));
+                $("#currentCity").text(inputCity + " - " + dayjs().format("MMM D, YYYY"));
                 $("#currentTemp").text("Temp: " + Math.round(data1.main.temp) + "° F");
                 $("#currentWind").text("Wind: " + Math.round(data1.wind.speed) + " - " + getDirection(data1.wind.deg));
                 $("#currentHumid").text("Humidity: " + data1.main.humidity + "%");
