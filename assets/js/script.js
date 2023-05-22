b22d238c008477d9d24f08ddcbca45fb9990d80f
skybreakbb <willistaylora@gmail.com>
2023-05-21 21:08:15 -0500

    wrote for loop to populate 5 day forecast fields

--- assets/js/script.js
+++ assets/js/script.js
@@ -117,7 +117,7 @@ $("#cityInputBtn").on("click", function() {
             .then(function (data1) {
                 console.log("This is from CURRENT weather request");
                 console.log(data1);
-                $("#currentCity").text(inputCity + " - " + dayjs().format("MMM D, YYYY"));
+                $("#currentCity").text(inputCity.charAt(0).toUpperCase() + inputCity.slice(1) + " - " + dayjs().format("MMM D, YYYY"));
                 $("#currentTemp").text("Temp: " + Math.round(data1.main.temp) + "° F");
                 $("#currentWind").text("Wind: " + Math.round(data1.wind.speed) + " - " + getDirection(data1.wind.deg));
                 $("#currentHumid").text("Humidity: " + data1.main.humidity + "%");
@@ -129,7 +129,6 @@ $("#cityInputBtn").on("click", function() {
     setTimeout(
         function getForecast() {
             var requestForecastUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + locLat + "&lon=" + locLon + "&units=imperial&appid=0dfeca27786a8a8c837f120f88c9a791";
-
             fetch(requestForecastUrl)
             .then(function (response) {
                 JSON.stringify(response);
@@ -138,31 +137,20 @@ $("#cityInputBtn").on("click", function() {
             .then(function (data2) {
                 console.log("This is from FORECAST weather request");
                 console.log(data2);
+                var iter = 1;
                 // 5 day forecast data population from fetched data2
-                // 1 of 5
-                $("#fiveTemp1").text("Temp: " + Math.round(data2.list[0].main.temp) + "° F");
-                $("#fiveWind1").text("Wind: " + Math.round(data2.list[0].wind.speed) + " - " + getDirection(data2.list[0].wind.deg));
-                $("#fiveHumid1").text("Humidity: " + data2.list[0].main.humidity + "%");
-                // 2 of 5
-                $("#fiveTemp2").text("Temp: " + Math.round(data2.list[0].main.temp) + "° F");
-                $("#fiveWind2").text("Wind: " + Math.round(data2.list[0].wind.speed) + " - " + getDirection(data2.list[0].wind.deg));
-                $("#fiveHumid2").text("Humidity: " + data2.list[0].main.humidity + "%");
-                // 3 of 5
-                $("#fiveTemp3").text("Temp: " + Math.round(data2.list[0].main.temp) + "° F");
-                $("#fiveWind3").text("Wind: " + Math.round(data2.list[0].wind.speed) + " - " + getDirection(data2.list[0].wind.deg));
-                $("#fiveHumid3").text("Humidity: " + data2.list[0].main.humidity + "%");
-                // 4 of 5
-                $("#fiveTemp4").text("Temp: " + Math.round(data2.list[0].main.temp) + "° F");
-                $("#fiveWind4").text("Wind: " + Math.round(data2.list[0].wind.speed) + " - " + getDirection(data2.list[0].wind.deg));
-                $("#fiveHumid4").text("Humidity: " + data2.list[0].main.humidity + "%");
-                // 5 of 5
-                $("#fiveTemp5").text("Temp: " + Math.round(data2.list[0].main.temp) + "° F");
-                $("#fiveWind5").text("Wind: " + Math.round(data2.list[0].wind.speed) + " - " + getDirection(data2.list[0].wind.deg));
-                $("#fiveHumid5").text("Humidity: " + data2.list[0].main.humidity + "%");
+                // console.log(data2.list[5].dt_txt);
+                for (var i = 0; i < data2.list.length; i++) {
+                    if (data2.list[i].dt_txt.includes("18:00:00") === true) {
+                        $("#fiveTemp" + iter).text("Temp: " + Math.round(data2.list[i].main.temp));
+                        $("#fiveWind" + iter).text("Wind: " + Math.round(data2.list[i].wind.speed));
+                        $("#fiveHumid" + iter).text("Temp: " + Math.round(data2.list[i].main.humidity));
+                        iter++;
+                    }   
+                }
             })
             // change button text back to 'Submit'
             $("#cityInputBtn").text("Submit");
-        }, 2000
+        }, 3000
     );
 });
-
